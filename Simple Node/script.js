4be215c0e793efcf1ca6afd8b2fb0b10bb0addbc
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-03-13 19:44:46 +0530

    first commit

---
+++ Simple Node/script.js
@@ -0,0 +1,5 @@
+var supervillains = require("supervillains");
+var superhero = require('superhero-name-library');
+var myvillains = supervillains.random();
+var myhero = superhero.random();
+console.log("My Super Hero is "+ myhero + " and My Super Villain is " + myvillains);
