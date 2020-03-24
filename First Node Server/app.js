5f40dffc70810d4c989357183e59f0e521528dc8
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-03-24 12:26:58 +0530

    Last Commit

--- First Node Server/app.js
+++ First Node Server/app.js
@@ -1,9 +1,16 @@
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write(' <html><head><title>My Page</title></head><body><form action = "/message" method = "POST"><input type = "text" name = "message"><button type = "submit">Send</button></form></body></html> ');
         return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'I Love You Vaaveee.. Ummaaaaahhh');
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
     }
     res.setHeader('Content-Type', 'text/html');
     res.write(' <html><head><title>My Page</title></head><body><h1>My first node js server response<h1></body></html> ');
