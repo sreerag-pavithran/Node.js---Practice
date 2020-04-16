085cf72c8b1e053fcbeb203a40d15a8f36f687e0
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-04-16 17:50:47 +0530

    ToDo List V1

--- Todo List V1/app.js
+++ Todo List V1/app.js
@@ -4,16 +4,47 @@ const ejs =  require("ejs")
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+var workItems = [];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"))
 
 app.get("/", function (req, res) {
   var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
+  var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-      kindOfDay: day
+      listTitle: day,
+      newListItems: items
   });
 
+app.post("/", function(req,res){
+  var item = req.body.newItem;
+
+  if(req.body.list === "Work"){
+      workItems.push(item);
+      res.redirect("/work");
+  }else {
+    items.push(item);
+    res.redirect("/")
+  }
+});
+app.get("/work", function(req,res){
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
 });
 
 app.listen(3000, function () {
